f22c2b893333b0ecccbb5e7dcaa4563a75a71946
Kajal Mishra <kmishra.slg@gmail.com>
2023-08-24 14:31:58 +0530

    Basics on JS

---
+++ basics/main.js
@@ -0,0 +1,11 @@
+// Primitive DT(Value) and Reference Type(reference)
+
+var a = 100
+var b = 50
+
+console.log(getSum(5,7))
+
+function getSum(num1, num2){
+    var sum = num1+num2
+    return sum
+}
